8a295e400f3fc891df3f4934c05eabb810b006c8
Rémi Vedrenne <remi.vedrenne.pro@gmail.com>
2025-05-09 15:45:33 +0200

    [CODE] Add Text mesh pro and start code user position

---
+++ Runup/Assets/Scripts/UserPosition.cs
@@ -0,0 +1,17 @@
+namespace KarpysDev.Runup
+{
+    using UnityEngine;
+
+    public class UserPosition : MonoBehaviour
+    {
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+    }
+}
